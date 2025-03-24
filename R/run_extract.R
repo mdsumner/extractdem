bc3fbe1d9e5a160a9ae6d0cbf6538ba20e8c5a4b
Michael Sumner <mdsumner@gmail.com>
2025-03-24 16:22:47 +1100

    grrr

--- R/run_extract.R
+++ R/run_extract.R
@@ -1,5 +1,5 @@
 extracted <- file.exists("longlat_points_dem.parquet")
-
+Sys.setenv("AWS_NO_SIGN_REQUEST" = "YES")
 if (!extracted) {
   track <- nanoparquet::read_parquet("longlat_points.parquet")
 library(xml2)
